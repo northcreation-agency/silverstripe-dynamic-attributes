6170dc54890529900b19a170c80ccc8798fc22fd
Viktor Åsbrink <viktor@kreationsbyran.se>
2021-12-09 14:47:01 +0100

    redo AttributeValue owner

--- src/model/AttributeValue.php
+++ src/model/AttributeValue.php
@@ -14,6 +14,7 @@ class AttributeValue extends DataObject
 
   private static $has_one = [
     'Attribute' => Attribute::class,
+    'OwnerItem' => DataObject::class,
   ];
 
   public function isLocalized()
