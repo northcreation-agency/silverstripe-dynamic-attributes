e4f3f80e6b594334df0afc10de78c2af823bad1e
Viktor Åsbrink <viktor@kreationsbyran.se>
2021-12-09 13:34:44 +0100

    update test setup

--- src/tests/AttributeTest.php
+++ src/tests/AttributeTest.php
@@ -1,11 +1,36 @@
 <?php
 
+namespace NorthCreationAgency\DynamicAttributes;
+
+use SilverStripe\Dev\Debug;
 use SilverStripe\Dev\SapphireTest;
 
 class AttributeTest extends SapphireTest
 {
-  public function testTest()
+
+  public function setUp(): void
   {
-    $this->assertNotContains("1", ["2"]);
+    parent::setUp();
+
+    for ($attr = 0; $attr < 5; $attr++) {
+      $attribute = Attribute::create([
+        'Key' => 'Attr' . "${$attr}",
+        'Title' => 'AttrTitle' . "${$attr}"
+      ]);
+      for ($val = 0; $val < 3; $val++) {
+        $value = AttributeValue::create([
+          'Value' => 'Attr' . "${$attr}" . 'Val' . "${$val}",
+          'LocalizedValue' => 'Attr' . "${$attr}" . 'LocalVal' . "${$val}",
+        ]);
+        $value->write();
+        $attribute->Values()->add($value);
+      }
+      $attribute->write();
+    }
+  }
+
+  public function testGetValue()
+  {
+    //TODO:: Implement tests
   }
 }
