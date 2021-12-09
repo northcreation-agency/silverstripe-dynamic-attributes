a61bdf56e0b745c1bc4dac1affcfe5a40aff0567
Viktor Åsbrink <viktor@kreationsbyran.se>
2021-12-09 12:11:07 +0100

    rename extension

---
+++ src/extensions/AttributeExtension.php
@@ -0,0 +1,30 @@
+<?php
+
+
+use SilverStripe\Forms\FieldList;
+use SilverStripe\ORM\DataExtension;
+
+class AttributeExtension extends DataExtension
+{
+  private static $has_one = [
+    'AttributeSet' => AttributeSet::class,
+  ];
+
+  private static $has_many = [
+    'AttributeValues' => AttributeValues::class,
+  ];
+
+  public function updateCMSFields(FieldList $fields)
+  {
+    $fields->removeByName('AttributeSet');
+    $fields->removeByName('AttributeValues');
+  }
+
+  public function getAttributes()
+  {
+    $set = $this->owner->AttributeSet();
+    $attributes = $set->Attributes();
+    $values = $attributes->Values()->filter(['OwnerID' => $this->owner->ID]);
+    return $values;
+  }
+}
