7dce5dac234531d53010174faf29667f4fbb3742
Viktor Åsbrink <viktor@kreationsbyran.se>
2022-05-31 14:51:56 +0200

    Update AttributeHolder to use AttributeExtension

--- src/model/AttributeHolder.php
+++ src/model/AttributeHolder.php
@@ -9,26 +9,4 @@ class AttributeHolder extends DataObject
 {
 
   private static $table_name = "Nca_AttributeHolder";
-
-  private static $has_one = [
-    'AttributeSet' => AttributeSet::class,
-  ];
-
-  private static $has_many = [
-    'AttributeValues' => AttributeValues::class,
-  ];
-
-  public function updateCMSFields(FieldList $fields)
-  {
-    $fields->removeByName('AttributeSet');
-    $fields->removeByName('AttributeValues');
-  }
-
-  public function getAttributes()
-  {
-    $set = $this->AttributeSet();
-    $attributes = $set->Attributes();
-    $values = $attributes->Values()->filter(['OwnerID' => $this->ID]);
-    return $values;
-  }
 }
